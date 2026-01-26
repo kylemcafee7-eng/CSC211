7c4579846e5698596cc1c72425d16062f6058d90
kylemcafee7-eng <kylemcafee7@gmail.com>
2026-01-25 22:24:53 -0700

    Create helloworld.php

---
+++ helloworld.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Hello, World!</title>
+</head>
+<body>
+<h1>My First PHP page</h1>
+<p>The following was created by PHP:
+<?php
+echo "Hello, world!";
+?>
+</p>
+</body>
+</html>
