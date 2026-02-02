760a1a6c8cd7eea3932431aa889420538c6ad007
kylemcafee7-eng <kylemcafee7@gmail.com>
2026-02-01 22:20:08 -0700

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Variables</title>
+</head>
+<body>
+<?php // Script 2.3 - Variables.php
+
+// An address: 
+$street = "422 North Basalt Road";
+$city = "Prescott";
+$state = "AZ";
+$zip = 89030;
+
+// Print the address: 
+print "<p>The address is <br>$street<br>$city $state $zip</p>";
+
+?>
+</body>
+</html>	
