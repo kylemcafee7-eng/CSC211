7aa73d44ebbf7d4f47ed9c0ada41cdb4574e2947
kylemcafee7-eng <kylemcafee7@gmail.com>
2026-02-02 23:32:02 -0700

    Add files via upload

---
+++ handle_form.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Your Feedback</title>
+</head>
+<body>
+
+<?php /* 3.3 Script handle_form.php
+This page receives the data from feedback.html.
+It will recieve: title, name, email, response, comments, and submit in $_POST.
+*/
+
+// Create shorthand versions of the variables:
+$title = $_POST['title'];
+$fname = $_POST['firstname'];
+$lname = $_POST['lastname'];
+$response = $_POST['response'];
+$comments = $_POST['comments'];
+
+// Print the received data: 
+print "<p>Thank you, $title $fname $lname,
+for your comments.</p>
+<p>You stated that you found this
+example to be '$response' and 
+added<br>'$comments'</p>";
+
+?>
+</body>
+</html>
