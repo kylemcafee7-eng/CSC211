89565dee6cd051f5efaf7d67c225832b0e762ba5
kylemcafee7-eng <kylemcafee7@gmail.com>
2026-02-09 12:56:23 -0700

    Add files via upload

---
+++ handle_calc.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Product Cost Calculator</title>
+	<style type="text/css">
+	.number { font-weight: bold; }
+	</style>
+</head>
+<body>
+
+<?php // Script 4.2 - handl_calc.php
+/*
+This script takes values from calculator.html and performs
+total cost and monthly payment calculations.
+*/
+
+// Address error handling, if you want (I don't)
+
+// Get the values from the $_POST array:
+$price = $_POST['price'];
+$quantity = $_POST['quantity'];
+$discount = $_POST['discount'];
+$tax = $_POST['tax'];
+$shipping = $_POST['shipping'];
+$payments = $_POST['payments'];
+
+// Cacl total: 
+$total = $price * $quantity;
+$total = $total + $shipping;
+$total = $total - $discount;
+
+// Tax rate
+$taxrate = $tax / 100;
+$taxrate = $taxrate + 1;
+
+// Apply tax rate 
+$total = $total * $taxrate;
+
+// Calculate the monthly payments:
+$monthly = $total / $payments;
+
+// Print results
+print "<p>You have selected to purchase:<br>
+<span class=\"number\">$quantity</span> widget(s) at <br>
+<span class=\"number\">$price</span> price each plus a <br>
+$<span class=\"number\">$shipping</span> shipping cost and a <br>
+<span class=\"number\">$tax</span> percent tax rate. <br>
+After your $<span class=\"number\">$discount</span> discount, the total is <br>
+$<span class=\"number\">$total</span>.<br>
+Divided over <span class=\"number\">$payments</span> monthly payments, that would be <br>
+$<span class=\"number\">$monthly</span> each.</p>";
+
+?>
+</body>
+</html>
+
