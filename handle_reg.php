a52441240ac2d806b8261854c474040eda25c4d2
kylemcafee7-eng <kylemcafee7@gmail.com>
2026-02-15 10:59:22 -0700

    Add files via upload

---
+++ handle_reg.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Registration</title>
+	
+
+</head>
+<body>
+<h1>Registration Results</h1>
+<?php /* Script 6.2 - handle_reg.php
+This script receives seven values from register.html:
+email, password, confirm, year, terms, color, submit*/
+
+// Flag variable to track success:
+$okay = true;
+
+// If there were no errors, print a success message:
+if($okay){
+	
+	print '<p>You have been successfully registered</p>';
+	
+}
+
+?>
+</body>
+</html>
