859df6f4030d7614480c55e585279cb5686f46de
kylemcafee7-eng <kylemcafee7@gmail.com>
2026-03-04 18:55:28 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php // Script 8.4 - index.php
+/* This is the home page for this site.
+   It uses templates to create the layout.
+*/
+
+// Include the header:
+include('templates/header.html');
+
+// Leave PHP to display the HTML:
+?>
+
+<h2>Welcome to a J.D. Salinger Fan Club!</h2>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+laborum.</p>
+
+<?php
+// Return to PHP and include the footer:
+include('templates/footer.html');
+?>
